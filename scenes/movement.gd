bc6af43e32a22b253a7f59637a299b034c59aa9f
963alseT <156242152+963alseT@users.noreply.github.com>
2026-03-08 21:40:14 +0000

    Collision stuff

--- scenes/movement.gd
+++ scenes/movement.gd
@@ -1,19 +1,38 @@
 extends CharacterBody2D
 
-@export var speed = 100  # pixels per second
+@export var speed = 50  # pixels per second
 @onready var sprite = $AnimatedSprite2D
+
+var last_direction = ""
 
 func _physics_process(delta):
 	var direction = Vector2.ZERO
 	
 	if Input.is_action_pressed("ui_up"):
 		direction.y -= 1
+		sprite.play("walk_back")
+		last_direction = "up"
 	if Input.is_action_pressed("ui_down"):
 		direction.y += 1
+		sprite.play("walk_front")
+		last_direction = "down"
 	if Input.is_action_pressed("ui_left"):
 		direction.x -= 1
+		sprite.play("walk_left")
+		last_direction = "left"
 	if Input.is_action_pressed("ui_right"):
 		direction.x += 1
+		sprite.play("walk_right")
+		last_direction = "right"
+	if direction == Vector2.ZERO:
+		if last_direction == "up":
+			sprite.play("idle_back")
+		if last_direction == "down":
+			sprite.play("idle_front")
+		if last_direction == "left":
+			sprite.play("idle_left")
+		if last_direction == "right":
+			sprite.play("idle_right")
 
 	direction = direction.normalized()
 	velocity = direction * speed
